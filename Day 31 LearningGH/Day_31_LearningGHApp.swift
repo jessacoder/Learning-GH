6f8d4f33be8facdd7cec551161f33efaad26a734
Jessica Yu <jessicatan365@gmail.com>
2023-05-03 09:41:21 -0400

    Initial Commit

---
+++ Day 31 LearningGH/Day_31_LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day_31_LearningGHApp.swift
+//  Day 31 LearningGH
+//
+//  Created by Jessa Yu on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Day_31_LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
